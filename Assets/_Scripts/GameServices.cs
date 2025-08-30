a7e8cde49cb99236fb80da7ca49c295e7d5ed54d
Md Kamruzzaman Tusher <8888749+tkamruzzaman@users.noreply.github.com>
2025-08-30 19:44:18 +0300

    sound and ui preliminary work

---
+++ Assets/_Scripts/GameServices.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GameServices : MonoBehaviour
+{
+    public static GameServices Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
