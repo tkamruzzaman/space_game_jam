c7878bf05ef8466d7506ff3754aef3f40ee34f40
Md Kamruzzaman Tusher <8888749+tkamruzzaman@users.noreply.github.com>
2025-08-31 13:01:05 +0300

    shooter contrain

--- Assets/Hasib/Scripts/Shooter.cs
+++ Assets/Hasib/Scripts/Shooter.cs
@@ -5,11 +5,22 @@ public class Shooter : MonoBehaviour
     public GameObject projectilePrefab;
     public Transform shootPoint;
 
+    int shootAmount = 5;
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(projectilePrefab, shootPoint.position, shootPoint.rotation);
+            if (shootAmount > 0)
+            {
+                Instantiate(projectilePrefab, shootPoint.position, shootPoint.rotation);
+                shootAmount--;
+            }
+            else
+            {
+                //GameOver
+            }
         }
     }
 }
+    
